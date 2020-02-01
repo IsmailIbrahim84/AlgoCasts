e9f049a089c24fc97278d7453c436280be516f91
IsmailIbrahim84 <beisso75@hotmail.com>
2020-02-02 01:15:17 +0200

    Adding C# cODE

---
+++ exercises/reversestring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace reversestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "apple";
+             char[] resutl;
+            string temp="";
+            resutl = input.ToCharArray();
+
+            for (int i=resutl.Length-1;i>=0;i--)
+            {
+                temp += resutl[i];
+            }
+           Console.WriteLine (temp);
+        }
+
+        
+    }
+}
